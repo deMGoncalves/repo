acdacfd134bd877569a65a2c54d057af02aa338e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-25 12:46:44 -0300

    chore(stylelintrc): instalacao e configuracao do stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,1 @@
-module.exports = {
-    "extends": ["stylelint-config-standard"],
-    "plugins": [
-      "stylelint-order"
-    ],
-    "rules": {
-      "order/order": [
-        "custom-properties",
-        "declarations"
-      ],
-      "order/properties-alphabetical-order": true
-    }
-  }
+module.exports = require('stylelintrc')
